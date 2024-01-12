00c9421be58a337472adc7d2a2856d44c98a1722
Carlos Henry Jr. <carlo.henrique37@gmail.com>
2024-01-12 17:14:06 -0300

    Capturando dados no Js

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 function Cadastrar(){
 
-    console.log(document.getElementById("NameUser").value)
-}
+    anoAtual = new Date();
 
-// nameUser = 
+    nameUser = document.getElementById("NameUser").value
+
+    ageUser = document.getElementById("AgeUser").value
+
+    dateBirth = document.getElementById("dateBirth").valueAsDate
+
+    console.log(nameUser)
+
+    console.log(ageUser)
+
+    if(anoAtual.getUTCFullYear() - dateBirth.getUTCFullYear() < 15){
+
+        console.log("Idade não permitida")
+
+    }else{
+
+        console.log(dateBirth.getUTCDate() + "/" + Number(dateBirth.getUTCMonth() + 1)  + "/" + dateBirth.getUTCFullYear())
+
+    }
+
+    if(document.getElementById("masculine").checked){
+
+        console.log("Masculino")
+
+    }else{
+        console.log("Feminino")
+
+    }
+}
